504de6bce5526f35b9c3cc284ae7b7dc638a389a
AramaAnastasya <Tyuiu.AramaAG@outlook.com>
2023-10-13 17:10:13 +0500

    TaskReview

---
+++ Tyuiu.AramaAG.Sprint0.TaskReview.V0.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.AramaAG.Sprint0.TaskReview.V0.Lib;
+
+namespace Tyuiu.AramaAG.Sprint0.TaskReview.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckCalc()
+        {
+            DataService ds = new DataService();
+            int x = 1;
+            int y = 2;
+            int z = 3;
+            int res = ds.Calc(x, y, z);
+            Assert.AreEqual(30, res);
+        }
+    }
+}
---
+++ Tyuiu.AramaAG.Sprint0.TaskReview.V0/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.AramaAG.Sprint0.TaskReview.V0.Lib;
+
+namespace Tyuiu.AramaAG.Sprint0.TaskReview.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #0 | Выполнил: Арама А.Г. | СМАРТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #0                                                               *");
+            Console.WriteLine("* Тема: Создание итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #0                                                              *");
+            Console.WriteLine("* Выполнил: Арама Анастасия Георгиевна | СМАРТб-23-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать консольную программу на C#, которая вычисляет значение         *");
+            Console.WriteLine("* выражения (x+t+z)*5.                                                    *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int x, y, z;
+            Console.WriteLine("* Введите число х");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("* Введите число y");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("* Введите число z");
+            z = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calc(x, y, z));
+            Console.ReadKey();
+        }
+    }
+}
